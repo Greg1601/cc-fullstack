fb5ebd13b500cb9b30115456a897620e2ebd0870
Greg1601 <greg1601@gmail.com>
2019-01-29 17:25:15 +0100

    séparation pages entreprises et talent

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,9 +10,9 @@ class HomeController extends AbstractController
 {
 
     /**
-     * @Route("/", name="home")
+     * @Route("/pro", name="homePro")
      */
-    public function indexAction()
+    public function homeProAction()
     {
 
         // récupération des éléments de JobOffer pour récupérer les derniers créés en priorité.
@@ -24,6 +24,27 @@ class HomeController extends AbstractController
         // stockage en variable des 3 premiers éléments du tableau récupéré pour affichage sur la page d'accueil (les 3 dernières offres    entrées en BDD seront affichées pour exemple sur la homepage)
         $lastThree = array_slice($jobs, 0, 3, true);
 
-        return $this->render('home.html.twig', ['jobs' => $lastThree]);
+        return $this->render('homePro.html.twig');
+        // return $this->render('home.html.twig', ['jobs' => $lastThree]);
     }
+
+    /**
+     * @Route("/talent", name="homeTalent")
+     */
+    public function homeTalentAction()
+    {
+
+        // récupération des éléments de JobOffer pour récupérer les derniers créés en priorité.
+        $jobs = $this ->getDoctrine()
+        ->getManager()
+        ->getRepository(JobOffer::class)
+        ->findBy(array(), array('id' => 'desc'));
+        
+        // stockage en variable des 3 premiers éléments du tableau récupéré pour affichage sur la page d'accueil (les 3 dernières offres    entrées en BDD seront affichées pour exemple sur la homepage)
+        $lastThree = array_slice($jobs, 0, 3, true);
+
+        // return $this->render('homePro.html.twig');
+        return $this->render('homeTalent.html.twig', ['jobs' => $lastThree]);
+    }
+
 }
